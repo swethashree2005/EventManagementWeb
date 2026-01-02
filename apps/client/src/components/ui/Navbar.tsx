cd8b40b347f74e6cac360c8f0dec73499111591b
swethashree2005 <swethasundaram2005@gmail.com>
2026-01-02 22:19:18 +0530

    finaloffinal6

--- apps/client/src/components/ui/Navbar.tsx
+++ apps/client/src/components/ui/Navbar.tsx
@@ -42,7 +42,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-2xl font-bold text-blue-600">
-              cventHub
+              EventHub
             </Link>
             <div className="hidden md:flex ml-10 space-x-8">
               <Link
